f31e72e7a6facfd6d854f043c52146554478a90a
Sandrine <nunubabila@gmail.com>
2021-01-06 20:14:36 -0800

    server'

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+var exphbs  = require('express-handlebars');
 const app = express();
 const port = 3000;
+
+// Set up view engine
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => { // access the route of express module
     res.send('Hey daters!');
