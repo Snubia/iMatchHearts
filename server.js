c36a9710c49611756c668602b2e85f6981152912
Sandrine <nunubabila@gmail.com>
2021-01-06 22:23:16 -0800

    port

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 var exphbs  = require('express-handlebars');
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 
 // Set up view engine
 
