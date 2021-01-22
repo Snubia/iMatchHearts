b887c15c94cc6fb0f70acf07b02293cb087a558d
Sandrine <nunubabila@gmail.com>
2021-01-22 10:56:04 -0800

    production env

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MongoDb: 'mongodb+srv://Amour:iMatchHearts@imatchhearts.ahjiz.mongodb.net/iMatchHearts?retryWrites=true&w=majority',
+    MongoDb: process.env.MONGO_DB,
     FacebookAppID: '2964988133731882',
     FacebookAppSecret: '11e00dc73c762002e5df13e70caa7fd2',
     GoogleClientID: '425898276771-ae4bdjiceam2l4tmt206eloe568do0jv.apps.googleusercontent.com',
