44e2315fc64349dbc3025bb9f866b0523c41fefe
Sandrine <nunubabila@gmail.com>
2021-01-22 12:44:59 -0800

    google prod

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,6 @@ module.exports = {
     MongoDb: process.env.MONGO_DB,
     FacebookAppID: process.env.FB_APP_ID,
     FacebookAppSecret: process.env.FB_APP_SECRET,
-    GoogleClientID: '425898276771-ae4bdjiceam2l4tmt206eloe568do0jv.apps.googleusercontent.com',
-    GoogleClientSecret: 'q0sihcC4mSlMfr7toSE5z0Hy'
+    GoogleClientID: process.env.GOOGLE_CLIENT_ID,
+    GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET
 }
