46371ce2a1f45d0c9b639e54727e2301d522d24e
Sandrine <nunubabila@gmail.com>
2021-01-21 18:20:40 -0800

    dev'

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+    MongoDb: 'mongodb+srv://Amour:iMatchHearts@imatchhearts.ahjiz.mongodb.net/iMatchHearts?retryWrites=true&w=majority',
+    FacebookAppID: '2964988133731882',
+    FacebookAppSecret: '11e00dc73c762002e5df13e70caa7fd2',
+    GoogleClientID: '425898276771-ae4bdjiceam2l4tmt206eloe568do0jv.apps.googleusercontent.com',
+    GoogleClientSecret: 'q0sihcC4mSlMfr7toSE5z0Hy'
+}
